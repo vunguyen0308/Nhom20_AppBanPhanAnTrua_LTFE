d910b7cc383d0a0abca6a2113fac15020a76e45a
Nguyễn Thạch Anh Vũ <91453600+vunguyen0308@users.noreply.github.com>
2022-07-18 23:37:07 +0700

    angular

---
+++ code/plugins/mdb-angular-onboarding/src/lib/onboarding-backdrop.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'mdb-onboarding-backdrop',
+  template: `<canvas class="onboarding-backdrop"></canvas>`,
+})
+export class MdbOnboardingBackdropComponent {}
