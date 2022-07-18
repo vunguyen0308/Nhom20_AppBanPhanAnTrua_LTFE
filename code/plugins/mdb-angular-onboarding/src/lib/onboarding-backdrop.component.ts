f1393af28342a660fca6fabed1064259b0d3f845
Nguyễn Thạch Anh Vũ <91453600+vunguyen0308@users.noreply.github.com>
2022-07-18 23:18:49 +0700

    Delete code directory

--- code/plugins/mdb-angular-onboarding/src/lib/onboarding-backdrop.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
-  selector: 'mdb-onboarding-backdrop',
-  template: `<canvas class="onboarding-backdrop"></canvas>`,
-})
-export class MdbOnboardingBackdropComponent {}
